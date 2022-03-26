a30cbdc6bcec8d57908bfa383fc17f24ee183f5e
Vallraven <matvey.phoen1x@yandex.ru>
2022-03-26 21:06:10 +0300

    перенес данные в пропсы

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,13 @@ import {Info, Search, Filter, List, Form} from "../../components/";
 import './App.scss';
 
 export const App = () => {
+    const date = [
+        {name: 'Иванов Иван', salary: 900, increase: false},
+        {name: 'Фёдоров Фёдор', salary: 800, increase: true},
+        {name: 'Семёнов Семён', salary: 1200, increase: false},
+        {name: 'Матвеев Матвей', salary: 900, increase: false}
+    ]
+
     return (
         <div className="app">
             <Info />
@@ -11,7 +18,7 @@ export const App = () => {
                 <Filter/>
             </div>
 
-            <List/>
+            <List date={date}/>
             <Form/>
         </div>
     );
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,12 +1,14 @@
 import {Element} from "../../components";
 import './List.scss';
 
-export const List = () => {
-    return (
-        <ul className="app-list list-group">
-            <Element/>
-            <Element/>
-            <Element/>
-        </ul>
-    );
+export const List = ({date}) => {
+    const elements = date && date.map((item, index) => {
+            return <Element key={index} {...item}/>
+        });
+
+        return (
+            <ul className="app-list list-group">
+                {elements}
+            </ul>
+        );
 }
