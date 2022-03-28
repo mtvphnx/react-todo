2700b35edc23d1c9a7e34305cc38f2424809494e
Vallraven <matvey.phoen1x@yandex.ru>
2022-03-28 21:39:45 +0300

    переписал функциональный компонент

--- src/components/Element/Element.jsx
+++ src/components/Element/Element.jsx
@@ -1,22 +1,50 @@
+import {Component} from 'react';
 import './Element.scss';
 
-export const Element = ({name, salary, increase}) => {
-    return (
-        <li className={`list-group-item d-flex justify-content-between ${increase ? 'increase' : null}`}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={`${salary}$`}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                        className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"/>
-                </button>
+export class Element extends Component {
+    constructor(props) {
+        super(props);
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash" />
-                </button>
-                <i className="fas fa-star" />
-            </div>
-        </li>
-    );
+        this.state = {
+            increase: this.props.increase,
+            like: false
+        }
+    }
+
+    toggleIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
+
+    toggleLike = () => {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+
+    render() {
+        const {name, salary} = this.props;
+        const {increase, like} = this.state;
+
+        return (
+            <li className={`list-group-item d-flex justify-content-between ${increase ? 'increase' : null} ${like ? 'like' : null}`}>
+                <span className="list-group-item-label" onClick={this.toggleLike}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={`${salary}$`}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                            className="btn-cookie btn-sm "
+                            onClick={this.toggleIncrease}>
+                        <i className="fas fa-cookie"/>
+                    </button>
+
+                    <button type="button"
+                            className="btn-trash btn-sm ">
+                        <i className="fas fa-trash" />
+                    </button>
+                    <i className="fas fa-star" />
+                </div>
+            </li>
+        );
+    }
 }
