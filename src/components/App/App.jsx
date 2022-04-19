4820e35876f914dafb41648295d1db49d1072608
Vallraven <vallraven@yandex.ru>
2022-04-19 19:44:21 +0300

    добавил запись

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,7 +10,8 @@ export class App extends Component {
                 {name: 'Иванов Иван', salary: 1900, increase: false, key: 0},
                 {name: 'Фёдоров Фёдор', salary: 800, increase: true, key: 1},
                 {name: 'Семёнов Семён', salary: 1200, increase: false, key: 2},
-                {name: 'Матвеев Матвей', salary: 900, increase: false, key: 3}
+                {name: 'Матвеев Матвей', salary: 900, increase: false, key: 3},
+                {name: 'Артёмов Артём', salary: 500, increase: true, key: 4}
             ],
             text: '',
             active: 'all'
